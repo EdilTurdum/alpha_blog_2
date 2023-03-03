553824b3ed0e74218430e84ee27565c9658e78c3
Edil Turdumambetov <edil13@mail.ru>
2023-03-03 21:59:41 +0300

    Add one-to-many association btwn users and articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,6 +19,7 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(article_params)
+        @article.user = User.first
         #render plain: @article
         
         if @article.save
